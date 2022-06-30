ac910012481009f72a7dea251880d1e359b260e4
HELLKYD <jakobr@posteo.de>
2022-06-30 17:07:25 +0200

    added Layers

--- NeuralNetwork.go
+++ NeuralNetwork.go
@@ -14,8 +14,8 @@ type Predictor interface {
 
 type Neuron struct {
 	weights []float64
-	inputs []float64
-	bias float64
+	inputs  []float64
+	bias    float64
 }
 
 func (n *Neuron) NewNeuron(inputs []float64) {
@@ -38,16 +38,16 @@ func (n *Neuron) computeInputs() float64 {
 	return out
 }
 
-func (n *Neuron) activate(inp float64) float64 {
+func (n *Neuron) activate(in float64) float64 {
 	a := Activator{}
-	a.NewActivator(func(in float64) float64{
+	a.NewActivator(func(in float64) float64 {
 		return 1 / (1 + math.Exp(-in))
 	}, n)
-	return a.activate(inp)
+	return a.activate(in)
 }
 
 type Activator struct {
-	aF func(float64) float64
+	aF   func(float64) float64
 	pred Predictor
 }
 
@@ -56,19 +56,50 @@ func (a *Activator) NewActivator(af func(float64) float64, pred Predictor) {
 	a.pred = pred
 }
 
-func (a *Activator) activate(inp float64) float64{
+func (a *Activator) activate(inp float64) float64 {
 	return a.aF(inp)
+}
+
+type Layer []Neuron
+
+func createLayer(neurons int, input []float64) *Layer {
+	var out Layer = make(Layer, 0)
+	for i := 0; i < neurons; i++ {
+		temp := Neuron{}
+		temp.NewNeuron(input)
+		out = append(out, temp)
+	}
+	return &out
+}
+
+func (l *Layer) computeInputs() []float64 {
+	var out []float64 = make([]float64, 0)
+	for _, v := range *l {
+		out = append(out, v.computeInputs())
+	}
+	return out
+}
+
+func (l *Layer) activate(in []float64) []float64 {
+	out := make([]float64, 0)
+	for pos, v := range *l {
+		out = append(out, v.activate(in[pos]))
+	}
+	return out
 }
 
 func main() {
 	inp := []float64{2.1, 1.2, 4.5}
-
-	n1 := Neuron{}
-	n1.NewNeuron(inp)
-
-	out := n1.computeInputs()
-	res := n1.activate(out)
+	var l1 *Layer = createLayer(3, inp)
+	out := l1.computeInputs()
+	res := l1.activate(out)
+	nout := Neuron{}
+	nout.NewNeuron(res)
+	noutout := nout.computeInputs()
+	resout := nout.activate(noutout)
+	fmt.Printf("l1: %v\n", l1)
 	fmt.Printf("out: %v\n", out)
 	fmt.Printf("res: %v\n", res)
+	fmt.Printf("resout: %v\n", resout)
 }
 
